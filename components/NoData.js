ea8dc3b2ad581008003c4988c9e51d6511c1755d
kush11 <kush.codingmart.com>
2020-02-20 17:54:19 +0530

    for loading data skeletonplaceholder added

--- components/NoData.js
+++ components/NoData.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 const NoData = () => {
-    return (
-        <View>
-            <Text>Loding</Text>
-        </View>
-    )
-}
-
-export default NoData
+  return (
+    <SafeAreaView>
+      <SkeletonPlaceholder>
+        <View style={styles}></View>
+        <View style={styles}></View>
+        <View style={styles}></View>
+        <View style={styles}></View>
+      </SkeletonPlaceholder>
+    </SafeAreaView>
+  );
+};
+const styles = {
+  borderWidth: 1,
+  height: 180,
+  width: null,
+  elevation: 5,
+  overflow: 'hidden',
+  borderRadius: 15,
+  borderColor: 'transparent',
+  shadowOffset: {width: 4, height: 4},
+  shadowColor: '#90a4ae',
+  margin: 10,
+};
+export default NoData;
