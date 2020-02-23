1702d2ee1eda9dfbef57135f6bf570b40cd3296f
kush11 <kush.codingmart.com>
2020-02-23 14:22:51 +0530

    Custom header added

--- source/WebView.js
+++ source/WebView.js
@@ -1,25 +1,64 @@
 import React, {PureComponent} from 'react';
-import {View} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import Webview from 'react-native-webview';
+import Icon from 'react-native-vector-icons/Ionicons'
 import Header from '../components/Header';
 
 export default class WebView extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
-    this.props.navigation.setOptions({ title: this.props.route.params.title })
+    this.props.navigation.setOptions({title: this.props.route.params.title});
   }
   backPress = () => {
     const {navigation} = this.props;
     navigation.goBack();
   };
   render() {
+    const Header = () => (
+      <View
+        style={{
+          paddingTop: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingBottom: 10,
+          backgroundColor: '#0c084c',
+          flexDirection: 'row',
+        }}>
+        <View
+          style={{flex: 0.15, justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableOpacity
+            style={
+              {
+                //   backgroundColor:'white'
+              }
+            }
+            onPress={() => {
+              this.props.navigation.goBack();
+            }}>
+            <Icon name="md-arrow-back" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 1}}>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 20,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}>
+            {title}
+          </Text>
+        </View>
+      </View>
+    );
     const {route} = this.props;
 
     const url = route.params.url;
     const title = route.params.title;
     return (
       <View style={{flex: 1}}>
+        <Header />
         {/* <Header title={title} nav={this.props.navigation} backPress={this.backPress} /> */}
         <Webview source={{uri: url}} />
       </View>
